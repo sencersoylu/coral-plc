6829bf9078a07f78c9a4417d9061ff118432f83a
Sencer SOYLU <sencer@soylubilgisayar.net>
2025-06-01 15:26:02 +0300

    Update index.js

--- index.js
+++ index.js
@@ -416,8 +416,10 @@ io.sockets.on('connection', (socket) => {
 			isWorking = 1;
 			const client = await openClientConnection();
 			console.log(data);
+			test = JSON.parse(data);
+			console.log(test);
 
-			let bufData = await writeMultipleData(data.startRegister, data.values);
+			let bufData = await writeMultipleData(test.startRegister, test.values);
 			await client.write(bufData);
 		} catch (err) {
 			console.log(err);
